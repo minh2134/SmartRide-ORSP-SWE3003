ee04211bd78b1f30ede5f7e82ee419c6762a44d1
Minh Pham <minh21345@gmail.com>
2025-03-24 05:20:10 +0700

    initialize a Spring boot project in server/

---
+++ server/src/test/java/com/osrp/smartride/SmartrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osrp.smartride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
