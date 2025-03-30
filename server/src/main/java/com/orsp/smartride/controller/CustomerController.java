a745ac3be08df4a83aa6808c11bf0e24a1c93815
Minh Pham <minh21345@gmail.com>
2025-03-30 07:45:00 +0700

    backend: rudimentary authentication support

--- server/src/main/java/com/orsp/smartride/controller/CustomerController.java
+++ server/src/main/java/com/orsp/smartride/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.orsp.smartride.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 	// TODO: below is test code, implement the real thing later
 	
-	@GetMapping("/customer")
+	@GetMapping("/customer/auth")
 	public String greeting() {
 		return "Hello, World!";
 	}
