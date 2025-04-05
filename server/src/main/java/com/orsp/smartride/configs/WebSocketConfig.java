2c2a3dfebf8d60ec547d516202e16484f966e1a2
Minh Pham <minh21345@gmail.com>
2025-04-06 04:10:06 +0700

    backend: hotfix: changed sockjs endpoint back to /ws to allow dual websocket and sockjs fallback

--- server/src/main/java/com/orsp/smartride/configs/WebSocketConfig.java
+++ server/src/main/java/com/orsp/smartride/configs/WebSocketConfig.java
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws");
-		registry.addEndpoint("/sockjs").withSockJS(); // SocketJS endpoint
+		registry.addEndpoint("/ws").withSockJS(); // SocketJS endpoint
 	}
 }
